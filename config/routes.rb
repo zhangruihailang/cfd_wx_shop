8925015e88a20bc84b9036b7a8532cc99df2d5df
zhangruihailang <zhangruihailang@163.com>
2015-12-04 06:30:32 +0000

    首页改版

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   mount WeixinRailsMiddleware::Engine, at: "/"
   resources :projects
 
-  root 'static_pages#goods_temai_mobile'
+  root 'static_pages#home'
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
